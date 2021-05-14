060392dc06fe7da9f3914f92629b543917c46062
Natália Rolim <natalia.rolim@celfocus.com>
2021-05-14 08:26:18 +0100

    create angular app and header style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge'
+import { SliderComponent } from './components/slider/slider.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatBadgeModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
